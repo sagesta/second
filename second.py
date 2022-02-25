2feaffb7d943141585b211afe954fc5cee3e9b3c
seisa <adebodun93@gmail.com>
2022-02-25 13:52:14 +0100

    calculating gross pay

--- second.py
+++ second.py
@@ -1,1 +1,5 @@
-
+#calculating gross pay from data inputted
+hour = float(input("please input the number of hours you worked:\n"))
+rate = float(input("also input the number of rate per hour for your work\n"))
+pay = hour * rate
+print ("Your pay is:",pay)
